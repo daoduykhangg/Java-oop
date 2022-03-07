ecd27a24a6c6404653988056d6c004d99af239f5
KhangD <Khang.DaoD@homecredit.vn>
2022-03-07 23:56:31 +0700

    HashMap Learning

--- src/Lession/lesson_01/HelloWorld.java
+++ src/Lession/lesson_01/HelloWorld.java
@@ -1,4 +1,4 @@
-package lesson_01;
+package Lession.lesson_01;
 
 public class HelloWorld {
     public static void main(String[] args) {
---
+++ src/Lession/lesson_05/HashMapLearning.java
@@ -0,0 +1,42 @@
+package Lession.lesson_05;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapLearning {
+    public static void main(String[] args) {
+        Map<Integer, String> emergencies = new HashMap<>();
+        emergencies.put(113, "Police Dept");
+        emergencies.put(114, "Fire Dept");
+        emergencies.put(115, "Hospital Dept");
+        emergencies.put(116, "Others");
+        emergencies.put(117, "Others");
+
+//        emergencies.replace(113,"CSCD");
+//        emergencies.replace(113,"Police Dept","CSCD_ABC");
+//        emergencies.remove(117,"Others Dept");
+//        System.out.println(emergencies.get(117));
+
+        Set<Integer> allKeys = emergencies.keySet();
+//        for (Integer key : allKeys) {
+//            if (emergencies.get(key).equals("Others")) {
+//                emergencies.remove(key);
+//            }
+//        }
+
+        Iterator<Map.Entry<Integer, String>> emergencyListIterator = emergencies.entrySet().iterator();
+        while (emergencyListIterator.hasNext()) {
+            Map.Entry<Integer, String> entry = emergencyListIterator.next();
+            String entryValue = entry.getValue();
+            if (entryValue.equals("Others")){
+                emergencyListIterator.remove();
+            }
+        }
+        for (Integer key : allKeys) {
+            System.out.println(emergencies.get(key));
+        }
+
+    }
+}
